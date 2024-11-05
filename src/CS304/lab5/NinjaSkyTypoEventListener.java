82591416d65cfbcf474b2c14a1e8b12e2e2e0456
Ahmed Mohamed <ahmedsuberyahooman2592002@gmail.com>
2024-11-05 03:58:32 +0200

    Ninja Game v2.1

--- src/CS304/lab5/NinjaSkyTypoEventListener.java
+++ src/CS304/lab5/NinjaSkyTypoEventListener.java
@@ -10,6 +10,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.io.IOException;
 import java.util.BitSet;
+import java.util.Random;
 
 public class NinjaSkyTypoEventListener implements GLEventListener, KeyListener {
     float screenHeight = 200;
@@ -18,29 +19,98 @@ public class NinjaSkyTypoEventListener implements GLEventListener, KeyListener {
     float xMin = -(screenWidth / 2f);
     float yMax = screenHeight / 2f;
     float yMin = -(screenHeight / 2f);
-    int x = 0, y = 0;
+    int xSoldier = 0, ySoldier = 0;
+    int xLetter = 0 , yLetter = (int)screenHeight-10;
+    int xStar = 0, yStar = 0;
+    float starRotate = 0;
     int animationIndex = 0;
+    int animationSpeed = 5; // Adjust for faster or slower animation (as num increase the anim slows)
+    int animationCounter = 0;
+    int letterIndex = 0;
 
-    String assetsFolderName = "CS304//Assets//Man";
+    String assetsFolderName = "CS304//Assets//Alphabet";
 
-    String[] textureNames = {"Man1.png","Man2.png","Man3.png","Man4.png","Back.png"};
+    String[] textureNames = {"Man1.png","Man2.png","Man3.png","Man4.png","ninjaStar.png","Back.png"};
+
+    String[] textureLettersNames = new String[26];
+    public void fillLetters(){
+        for (int i = 0; i < 26; i++) {
+            char letter = (char) ('a' + i);
+            textureLettersNames[i] = letter + ".png";
+        }
+    }
+
     TextureReader.Texture[] texture = new TextureReader.Texture[textureNames.length];
+    TextureReader.Texture[] textureLetter = new TextureReader.Texture[textureLettersNames.length];
+
     int[] textures = new int[textureNames.length];
+    int[] texturesLetters = new int[textureLettersNames.length];
+
 
     @Override
     public void init(GLAutoDrawable glAutoDrawable) {
+        fillLetters();
+
         GL gl = glAutoDrawable.getGL();
         gl.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
         gl.glEnable(GL.GL_TEXTURE_2D);
         gl.glBlendFunc(GL.GL_SRC_ALPHA,GL.GL_ONE_MINUS_SRC_ALPHA);
+        generateTextures(textureNames,texture,textures,gl);
+        generateTextures(textureLettersNames,textureLetter,texturesLetters,gl);
+
+    }
+
+    @Override
+    public void display(GLAutoDrawable glAutoDrawable) {
+        yLetter -= 1; // here the letter going down
+        GL gl = glAutoDrawable.getGL();
+        gl.glClear(GL.GL_COLOR_BUFFER_BIT);
+        gl.glLoadIdentity();
+        {
+            DrawBackground(gl);
+            handleKeyPress();
+            DrawSprite(gl, xSoldier, ySoldier, textures, animationIndex, 0, 2); // draw the soldier
+        }//^Soldier & back-ground
+        {
+            if (isStarFlying) {
+                yStar += 1;
+                DrawSprite(gl, xStar, yStar, textures, 4, starRotate += 10, 5); // draw our star
+                if (yStar > yMax - 10) {
+                    isStarFlying = false;
+                }
+            }
+        }//^Ninja Star
+        {
+            DrawSprite(gl, xLetter, yLetter, texturesLetters, letterIndex, 0, 1); // draw letters
+            double dist = sqrdDistance(xStar,yStar,xLetter,yLetter);
+            double radii = Math.pow(0.5*0.1*yMax+0.5*0.1*yMax,2);
+            boolean isCollided = dist<=radii;
+            if (yLetter < yMin + 10 || isCollided) {
+                xLetter = (int) (Math.random() * (screenWidth-10) + 1) - (int) xMax-5; // random falling letters positions
+                yLetter = (int) yMax; // always comes from the sky
+                letterIndex = (int) (Math.random() * 26); // picking random letter
+                isStarFlying = false;
+            }
+        }//^Letters
+    }
+
+    @Override
+    public void reshape(GLAutoDrawable glAutoDrawable, int i, int i1, int i2, int i3) {
+
+    }
+    public double sqrdDistance(int x, int y, int x1, int y1){
+        return Math.pow(x-x1,2)+Math.pow(y-y1,2);
+    }
+    @Override
+    public void displayChanged(GLAutoDrawable glAutoDrawable, boolean b, boolean b1) {
+
+    }
+    public void generateTextures(String[] textureNames, TextureReader.Texture[] texture, int[] textures, GL gl) {
         gl.glGenTextures(textureNames.length,textures,0);
         for (int i = 0; i < textureNames.length; i++) {
             try {
                 texture[i] = TextureReader.readTexture(assetsFolderName + "//" + textureNames[i] , true);
-                System.out.println(assetsFolderName + "//" + textureNames[i]);
                 gl.glBindTexture(GL.GL_TEXTURE_2D, textures[i]);
-
-//                mipmapsFromPNG(gl, new GLU(), texture[i]);
                 new GLU().gluBuild2DMipmaps(
                         GL.GL_TEXTURE_2D,
                         GL.GL_RGBA, // Internal Texel Format,
@@ -54,37 +124,15 @@ public class NinjaSkyTypoEventListener implements GLEventListener, KeyListener {
                 e.printStackTrace();
             }
         }
-
     }
-
-    @Override
-    public void display(GLAutoDrawable glAutoDrawable) {
-        GL gl = glAutoDrawable.getGL();
-        gl.glClear(GL.GL_COLOR_BUFFER_BIT);
-        gl.glLoadIdentity();
-        DrawBackground(gl);
-        handleKeyPress();
-        animationIndex %= 4;
-        DrawSprite(gl, x, y, animationIndex, 1);
-    }
-
-    @Override
-    public void reshape(GLAutoDrawable glAutoDrawable, int i, int i1, int i2, int i3) {
-
-    }
-
-    @Override
-    public void displayChanged(GLAutoDrawable glAutoDrawable, boolean b, boolean b1) {
-
-    }
-    public void DrawSprite(GL gl,int x, int y, int index, float scale){
+    public void DrawSprite(GL gl,int x, int y,int[] textures, int index,float rotate, float scale){
         gl.glEnable(GL.GL_BLEND);
         gl.glBindTexture(GL.GL_TEXTURE_2D, textures[index]);	// Turn Blending On
 
         gl.glPushMatrix();
         gl.glTranslated( x/xMax, y/yMax, 0);
-        gl.glScaled(0.1*scale, 0.1*scale, 1);
-        //System.out.println(x +" " + y);
+        gl.glScaled(0.05*scale, 0.05*scale, 1);
+        gl.glRotated(rotate,0,0,1);
         drawFullScreenQuad(gl);
         gl.glPopMatrix();
 
@@ -116,33 +164,40 @@ public class NinjaSkyTypoEventListener implements GLEventListener, KeyListener {
     public void handleKeyPress() {
         boolean flag = false;
         if (isKeyPressed(KeyEvent.VK_LEFT)) {
-            if (x > xMin+5) {
-                x--;
+            if (xSoldier > xMin+5) {
+                xSoldier--;
             }
             flag = true;
         }
         if (isKeyPressed(KeyEvent.VK_RIGHT)) {
-            if (x < xMax-5) {
-                x++;
+            if (xSoldier < xMax-5) {
+                xSoldier++;
             }
             flag = true;
         }
         if (isKeyPressed(KeyEvent.VK_DOWN)) {
-            if (y > yMin+10) {
-                y--;
+            if (ySoldier > yMin+10) {
+                ySoldier--;
             }
             flag = true;
         }
         if (isKeyPressed(KeyEvent.VK_UP)) {
-            if (y < yMax-10) {
-                y++;
+            if (ySoldier < yMax-10) {
+                ySoldier++;
             }
             flag = true;
         }
-        if (flag) {
-            animationIndex++;
+        if (flag) { // only increment if movement is detected
+            animationCounter++;
+            if (animationCounter >= animationSpeed) {
+                animationIndex++;
+                animationIndex %= 4;
+                animationCounter = 0;
+            }
         }
     }
+
+    boolean isStarFlying = false;
     public BitSet keyBits = new BitSet(256);
     @Override
     public void keyTyped(KeyEvent e) {
@@ -153,6 +208,12 @@ public class NinjaSkyTypoEventListener implements GLEventListener, KeyListener {
     public void keyPressed(KeyEvent e) {
         int keyCode = e.getKeyCode();
         keyBits.set(keyCode);
+
+        if (keyCode == KeyEvent.VK_SPACE && !isStarFlying) {
+            isStarFlying = true;
+            xStar = xSoldier; // Initialize x position at soldier's position
+            yStar = ySoldier + 10; // Set the star just above the soldier
+        }
     }
 
     @Override
